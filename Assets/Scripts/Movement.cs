2ef3c8f0bad4a0f0be9665fdcdbe22cf5e55fdbd
TheNuttyGamer <33847045+TheNuttyGamer@users.noreply.github.com>
2017-12-02 10:00:57 +0000

    Added Movement system

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    //Declaring Variables
+    [SerializeField]
+    private float speed;
+
+    private Rigidbody2D rb2d;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	 if (Input.GetKey(KeyCode.A))
+        {
+            rb2d.velocity = new Vector3(-1 * speed, 0);
+        }
+
+     if (Input.GetKey(KeyCode.D))
+        {
+            rb2d.velocity = new Vector3(1 * speed, 0);
+        }
+	}
+}
